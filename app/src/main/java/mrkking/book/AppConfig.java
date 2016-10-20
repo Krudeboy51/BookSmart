790389547c2f09b252443fe3c2758166c99633cb
Kory King <kking9351@gmail.com>
2016-10-20 03:24:11 -0400

    v1

---
+++ app/src/main/java/mrkking/book/AppConfig.java
@@ -0,0 +1,12 @@
+package mrkking.book;
+
+/**
+ * Created by Kory on 10/17/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://mrkking.com/badman/php/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://mrkking.com/badman/php/Register.php";
+}
